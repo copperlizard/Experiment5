6171f2bdf49f75ba7e7d14260233497bbb43b1e6
Cam McKasson <cam@mckassons.com>
2016-08-31 17:07:12 -0700

    working on hlapi

---
+++ Assets/_Scripts/LLAPI/LLAPIPlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LLAPIPlayerController : MonoBehaviour
+{
+    private float m_v, m_h;
+    private bool m_fire;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GetInput();
+	}
+
+    void GetInput ()
+    {
+        m_v = Input.GetAxis("Vertical");
+        m_h = Input.GetAxis("Horizontal");
+        m_fire = Input.GetButton("Jump"); //change this later!!!
+    }
+}
---
+++ Assets/_Scripts/LLAPI/UserCommand.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class UserCommand
+{
+    public int m_commandFrame;
+
+    // Client side interpolation time
+    public float m_clientInterpTime;
+
+    // Current player orientation
+    public Quaternion m_clientRotation;
+
+    // User movement input
+    public float m_v, m_h;
+
+    // Buttons
+    public bool m_fire;
+}
+
+/*
+public class UserCommand : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+*/
